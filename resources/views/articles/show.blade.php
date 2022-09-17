913bc15bdc5a1a1d59afb3e4e3f757d857e9fd17
velmouth-star <shanpuyangshu@gmail.com>
2022-09-17 11:55:44 +0900

    README.meの変更, show.blade.phpの記述

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,1 +1,18 @@
-{{ dd($article) }}
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>{{ $article->title }}</h1>
+    <div>
+        {!! Str::markdown($article->body, ['html_input' => 'escape']) !!}
+    </div>
+</body>
+
+</html>
