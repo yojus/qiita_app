3c57a7a76e9e7a4b3726ab20769151ad7af5f34e
velmouth-star <shanpuyangshu@gmail.com>
2022-09-17 16:44:02 +0900

    show.blade.php 削除ボタン作成

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -13,6 +13,11 @@
     <button type="button" onclick="location.href='{{ route('articles.index') }}' ">一覧へ戻る</button>
     @if ($article->user->permanent_id == $user->permanent_id)
         <button type="button" onclick="location.href='{{ route('articles.edit', $article->id) }}' ">編集する</button>
+        <button type="button" onclick="if(!confirm('本当に削除していいですか？')){return false};">削除する</button>
+        <form action="{{ route('articles.destroy', $article->id) }}" method="post" id="delete-form"></form>
+        @csrf
+        @method('DELETE')
+        </form>
     @endif
     <h1>{{ $article->title }}</h1>
     {{-- <div> --}}
